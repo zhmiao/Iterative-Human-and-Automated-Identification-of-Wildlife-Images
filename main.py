c9aa9367e4cd4fa5fd2f4c6fbb7ed7625e85009e
zhmiao <zhongqi.miao@berkeley.edu>
2019-11-05 01:58:18 -0800

    update with logger and training

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 import os
+import shutil
 import yaml
+from  datetime import datetime
+import logging
 from argparse import ArgumentParser
 
 from src.algorithms.utils import get_algorithm
 
-data_root = '/home/zhmiao/datasets/ecology'
-
+###################
+# Parse arguments #
+###################
 parser = ArgumentParser()
 parser.add_argument('--config', default='./configs/plain_resnet_041119.yaml')
 parser.add_argument('--gpu', default='0')
+parser.add_argument('--evaluate', default=False, action='store_true')
+parser.add_argument('--clear_logs', default=False, action='store_true')
 args = parser.parse_args()
 
+
+###########
+# Set gpu #
+###########
 os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
 
+# Load configurations
 with open(args.config) as f:
     config = yaml.load(f)
 for k, v in config.items():
     setattr(args, k, v)
 
+#################
+# Create logger #
+#################
+log_root = './log'
+if not os.path.exists(log_root):
+    os.mkdir(log_root)
+log_file = os.path.join(log_root, '{}_{}.log'.format(args.algorithm, args.conf_id))
+logging.basicConfig(format='%(levelname)s: %(message)s')
+logger = logging.getLogger('MAIN')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(log_file, mode='w')
+logger.addHandler(handler)
+setattr(args, 'logger', logger)
+
+##############
+# algorithms #
+##############
 alg = get_algorithm(args.algorithm, args)
+if not args.evaluate:
+    alg.train()
+
