7cfa83802559c77ba9e97e1a9a5c5829ac0dae60
zhmiao <zhongqi.miao@berkeley.edu>
2019-11-05 01:57:26 -0800

    update plain resnet algorithm with training functions

--- src/algorithms/plain_resnet.py
+++ src/algorithms/plain_resnet.py
@@ -20,19 +20,18 @@ class PlainResNet:
 
     def __init__(self, args):
 
-    # def __init__(self, dataset_root, dataset_name='CCT_cis', model_name='PlainResNetClassifier'):
-
-
         self.num_epochs = args.num_epochs
+        self.log_interval = args.log_interval
+        self.logger = args.logger
 
         #######################################
         # Setup data for training and testing #
         #######################################
         self.trainloader = load_dataset(name=args.dataset_name, dset='train', rootdir=args.dataset_root,
-                                        batch_size=64, shuffle=True, num_workers=1)
+                                        batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
         self.testloader = load_dataset(name=args.dataset_name, dset='test', rootdir=args.dataset_root,
-                                       batch_size=64, shuffle=True, num_workers=1)
+                                       batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
         ###########################
         # Setup cuda and networks #
@@ -56,24 +55,65 @@ class PlainResNet:
              'momentum': args.momentum_classifier,
              'weight_decay': args.weight_decay_classifier}
         ]
-        opt_net = optim.SGD(net_optim_params_list)
+        self.opt_net = optim.SGD(net_optim_params_list)
+        # TODO: scheduler
 
-    def train_epoch(self):
-        pass
+    def train_epoch(self, epoch):
+
+        self.net.train()
+
+        N = len(self.trainloader)
+
+        for batch_idx, (data, labels) in enumerate(self.trainloader):
+
+            # log basic adda train info
+            info_str = '[Train plain] Epoch: {} [{}/{} ({:.2f}%)] '.format(epoch, batch_idx,
+                                                                           N, 100 * batch_idx / N)
+
+            ########################
+            # Setup data variables #
+            ########################
+            data, labels = data.cuda(), labels.cuda()
+
+            data.require_grad = False
+            labels.require_grad = False
+
+            ####################
+            # Forward and loss #
+            ####################
+            # forward
+            feats = self.net.feature(data)
+            logits = self.net.classifier(feats)
+            # calculate loss
+            loss = self.net.criterion_cls(logits, labels)
+
+            #############################
+            # Backward and optimization #
+            #############################
+            # zero gradients for optimizer
+            self.opt_net.zero_grad()
+            # loss backpropagation
+            loss.backward()
+            # optimize step
+            self.opt_net.step()
+
+            ###########
+            # Logging #
+            ###########
+            if batch_idx % self.log_interval == 0:
+                # compute discriminator acc
+                preds = logits.argmax(dim=1)
+                acc = (preds == labels).float().mean()
+                # log discriminator update info
+                info_str += 'Acc: {:0.1f} Xent: {:.3f}'.format(acc.item() * 100, loss.item())
+                self.logger.info(info_str)
 
     def test_epoch(self):
         pass
 
     def train(self):
-        pass
-        ##############
-        # Train Adda #
-        ##############
-        # for epoch in range(num_epoch):
-        #     err = train_epoch(train_src_data, train_tgt_data, net, opt_net, opt_dis, epoch)
-        #     if err == -1:
-        #         print("No suitable discriminator")
-        #         break
+        for epoch in range(self.num_epochs):
+            self.train_epoch(epoch)
 
     def save_model(self):
         pass
