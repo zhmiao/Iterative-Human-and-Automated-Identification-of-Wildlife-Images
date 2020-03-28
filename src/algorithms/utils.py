7e26747f9368ef266950f4ab9c8c6da2b2a4055b
zhmiao <zhongqi.miao@berkeley.edu>
2020-03-28 01:04:03 -0700

    stage 1 evaluation metrics with f1 known set and unknown percentage

--- src/algorithms/utils.py
+++ src/algorithms/utils.py
@@ -1,6 +1,7 @@
+import numpy as np
+
+
 algorithms = {}
-
-
 def register_algorithm(name):
 
     """
@@ -56,4 +57,84 @@ class Algorithm:
 
     def save_model(self):
         pass
+
+
+def f_measure(preds, labels):
+    # f1 score for openset evaluation with close set accuracy
+    true_pos = 0.
+    false_pos = 0.
+    false_neg = 0.
+
+    for i in range(len(labels)):
+        true_pos += 1 if preds[i] == labels[i] and labels[i] != -1 else 0
+        false_pos += 1 if preds[i] != labels[i] and labels[i] != -1 else 0
+        false_neg += 1 if preds[i] != labels[i] and labels[i] == -1 else 0
+
+    precision = true_pos / (true_pos + false_pos)
+    recall = true_pos / (true_pos + false_neg)
+    return 2 * ((precision * recall) / (precision + recall + 1e-12))
+
+
+def confident_metrics(preds, labels, unique_classes, class_counts):
+    # Confident metrics
+    class_correct_confident = np.array([0. for _ in range(len(np.unique(labels)) - 1)])
+    class_select_confident = np.array([0. for _ in range(len(np.unique(labels)) - 1)])
+    preds_confident = preds[preds != -1]
+    labels_confident = labels[preds != -1]
+
+    false_pos_counts = 0.
+
+    # compute correct predictions in confident preds
+    for i in range(len(preds_confident)):
+        pred = preds_confident[i]
+        label = labels_confident[i]
+
+        if label != -1:
+            # Confident known accuracy
+            if pred == label:
+                class_correct_confident[label] += 1
+            class_select_confident[label] += 1
+        else:
+            # Counts of unknown in confident set
+            false_pos_counts += 1
+
+    # Record per class accuracies for confident data
+    class_acc_confident = class_correct_confident / class_select_confident
+    class_percent_confident = class_select_confident / class_counts[1:]
+    false_pos_percent = false_pos_counts / len(labels_confident)
+
+    return class_acc_confident, class_percent_confident, false_pos_percent
+
+
+def unknown_metrics(preds, labels):
+    # Unknown metrics
+    correct_unknown = 0.
+    total_unknown = 0.
+    preds_unknown = preds[labels == -1]
+    labels_unknown = labels[labels == -1]
+
+    for i in range(len(preds_unknown)):
+        pred = preds_unknown[i]
+        label = labels_unknown[i]
+        # record correctly picked unconfident samples
+        if pred == label:
+            correct_unknown += 1
+        # record all unconfident samples
+        total_unknown += 1
+
+    percent_unknown = correct_unknown / total_unknown
+
+    return percent_unknown
+
+
+def stage_1_metric(preds, labels, unique_classes, class_counts):
+    # f1
+    f1 = f_measure(preds, labels)
+    # Confident
+    class_acc_confident, class_percent_confident, false_pos_percent = confident_metrics(preds, labels, unique_classes, class_counts)
+    # Open
+    percent_unknown = unknown_metrics(preds, labels)
+    return f1, class_acc_confident, class_percent_confident, false_pos_percent, percent_unknown
+
+
 
