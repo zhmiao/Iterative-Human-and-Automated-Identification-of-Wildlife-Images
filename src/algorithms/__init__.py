f7fb2d340a3186f75ee663be1a11261c1de7f173
zhmiao <zhongqi.miao@berkeley.edu>
2020-04-01 18:04:34 -0700

    initialization of memory stage 2 algorithm

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -3,3 +3,4 @@ from . import plain_resnet
 from . import plain_stage_1
 from . import memory_stage_1
 from . import plain_stage_2
+from . import memory_stage_2
---
+++ src/algorithms/memory_stage_2.py
@@ -0,0 +1,258 @@
+import os
+import numpy as np
+from datetime import datetime
+from tqdm import tqdm
+
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+
+from .utils import register_algorithm, Algorithm, stage_1_metric
+from src.data.utils import load_dataset
+from src.data.class_indices import class_indices
+from src.models.utils import get_model
+
+from .plain_resnet import FineTuneResNet
+
+
+def load_data(args, conf_preds, unknown_only=False):
+
+    """
+    Dataloading function. This function can change alg by alg as well.
+    """
+
+    print('Using class indices: {} \n'.format(class_indices[args.class_indices]))
+
+    cls_idx = class_indices[args.class_indices]
+
+    trainloader = load_dataset(name=args.dataset_name,
+                               class_indices=cls_idx,
+                               dset='train',
+                               transform='train',
+                               split=args.train_split,
+                               rootdir=args.dataset_root,
+                               batch_size=args.batch_size,
+                               shuffle=True,
+                               num_workers=args.num_workers,
+                               conf_preds=conf_preds,
+                               unknown_only=unknown_only)
+
+    # Use replace S1 to S2 for evaluation
+    testloader = load_dataset(name=args.dataset_name,
+                              class_indices=cls_idx,
+                              dset='train',
+                              transform='eval',
+                              split=None,
+                              rootdir=args.dataset_root,
+                              batch_size=args.batch_size,
+                              shuffle=False,
+                              num_workers=args.num_workers,
+                              conf_preds=None,
+                              unknown_only=False)
+
+    # Use replace S1 to S2 for evaluation
+    valloader = load_dataset(name=args.dataset_name,
+                             class_indices=cls_idx,
+                             dset='train',
+                             transform='eval',
+                             split=None,
+                             rootdir=args.dataset_root,
+                             batch_size=args.batch_size,
+                             shuffle=False,
+                             num_workers=args.num_workers,
+                             conf_preds=None,
+                             unknown_only=False)
+
+    return trainloader, testloader, valloader
+
+
+@register_algorithm('MemoryStage2')
+class MemoryStage2(Algorithm):
+
+    """
+    Overall training function.
+    """
+
+    name = 'MemoryStage2'
+    net = None
+    opt_net = None
+    scheduler = None
+
+    def __init__(self, args):
+        super(MemoryStage2, self).__init__(args=args)
+
+        # Training epochs and logging intervals
+        self.num_epochs = args.num_epochs
+        self.log_interval = args.log_interval
+        self.conf_preds = list(np.fromfile(args.conf_preds).astype(int))
+
+        #######################################
+        # Setup data for training and testing #
+        #######################################
+        self.trainloader, self.testloader, self.valloader = load_data(args, self.conf_preds, unknown_only=True)
+        _, self.train_class_counts = self.trainloader.dataset.class_counts_cal()
+
+    def set_train(self):
+        ###########################
+        # Setup cuda and networks #
+        ###########################
+        # setup network
+        # The only thing different from plain resnet is that init_feat_only is set to true now
+        self.logger.info('\nGetting {} model.'.format(self.args.model_name))
+        self.net = get_model(name=self.args.model_name, num_cls=len(class_indices[self.args.class_indices]),
+                             weights_init=self.args.weights_init, num_layers=self.args.num_layers, init_feat_only=True)
+
+        ######################
+        # Optimization setup #
+        ######################
+        # Setup optimizer parameters for each network component
+        net_optim_params_list = [
+            {'params': self.net.feature.parameters(),
+             'lr': self.args.lr_feature,
+             'momentum': self.args.momentum_feature,
+             'weight_decay': self.args.weight_decay_feature},
+            {'params': self.net.classifier.parameters(),
+             'lr': self.args.lr_classifier,
+             'momentum': self.args.momentum_classifier,
+             'weight_decay': self.args.weight_decay_classifier}
+        ]
+        # Setup optimizer and optimizer scheduler
+        self.opt_net = optim.SGD(net_optim_params_list)
+        self.scheduler = optim.lr_scheduler.StepLR(self.opt_net, step_size=self.args.step_size, gamma=self.args.gamma)
+
+    def set_eval(self):
+        ###############################
+        # Load weights for evaluation #
+        ###############################
+        self.logger.info('\nGetting {} model.'.format(self.args.model_name))
+        self.logger.info('\nLoading from {}'.format(self.weights_path))
+        self.net = get_model(name=self.args.model_name, num_cls=len(class_indices[self.args.class_indices]),
+                             weights_init=self.weights_path, num_layers=self.args.num_layers, init_feat_only=False)
+
+    def train_epoch(self, epoch):
+
+        self.net.train()
+
+        N = len(self.trainloader)
+
+        for batch_idx, (data, labels, _) in enumerate(self.trainloader):
+
+            # log basic adda train info
+            info_str = '[Train plain] Epoch: {} [{}/{} ({:.2f}%)] '.format(epoch, batch_idx,
+                                                                           N, 100 * batch_idx / N)
+
+            ########################
+            # Setup data variables #
+            ########################
+            data, labels = data.cuda(), labels.cuda()
+
+            data.require_grad = False
+            labels.require_grad = False
+
+            ####################
+            # Forward and loss #
+            ####################
+            # forward
+            feats = self.net.feature(data)
+            logits = self.net.classifier(feats)
+            # calculate loss
+            loss = self.net.criterion_cls(logits, labels)
+
+            #############################
+            # Backward and optimization #
+            #############################
+            # zero gradients for optimizer
+            self.opt_net.zero_grad()
+            # loss backpropagation
+            loss.backward()
+            # optimize step
+            self.opt_net.step()
+
+            ###########
+            # Logging #
+            ###########
+            if batch_idx % self.log_interval == 0:
+                # compute overall acc
+                preds = logits.argmax(dim=1)
+                acc = (preds == labels).float().mean()
+                # log update info
+                info_str += 'Acc: {:0.1f} Xent: {:.3f}'.format(acc.item() * 100, loss.item())
+                self.logger.info(info_str)
+
+        self.scheduler.step()
+
+    def train(self):
+
+        best_acc = 0.
+
+        for epoch in range(self.num_epochs):
+
+            # Training
+            self.train_epoch(epoch)
+
+            # Validation
+            self.logger.info('\nValidation.')
+            val_acc_mac = self.evaluate(self.valloader)
+            if val_acc_mac > best_acc:
+                self.net.update_best()
+
+        self.save_model()
+
+    def evaluate_epoch(self, loader):
+
+        self.net.eval()
+
+        # Get unique classes in the loader and corresponding counts
+        loader_uni_class, eval_class_counts = loader.dataset.class_counts_cal()
+        class_correct = np.array([0. for _ in range(len(eval_class_counts))])
+
+        # Forward and record # correct predictions of each class
+        with torch.set_grad_enabled(False):
+
+            for data, labels in tqdm(loader, total=len(loader)):
+
+                # setup data
+                data, labels = data.cuda(), labels.cuda()
+                data.require_grad = False
+                labels.require_grad = False
+
+                # forward
+                feats = self.net.feature(data)
+                logits = self.net.classifier(feats)
+
+                # compute correct
+                preds = logits.argmax(dim=1)
+                for i in range(len(preds)):
+                    pred = preds[i]
+                    label = labels[i]
+                    if pred == label:
+                        class_correct[label] += 1
+
+        # Record per class accuracies
+        class_acc = class_correct[loader_uni_class] / eval_class_counts[loader_uni_class]
+        overall_acc = class_correct.sum() / eval_class_counts.sum()
+        eval_info = '{} Per-class evaluation results: \n'.format(datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
+        for i in range(len(class_acc)):
+            eval_info += 'Class {} (train counts {}): {:.3f} \n'.format(i, self.train_class_counts[loader_uni_class][i],
+                                                                        class_acc[i] * 100)
+
+        # Record missing classes in evaluation sets if exist
+        missing_classes = list(set(loader.dataset.class_indices.values()) - set(loader_uni_class))
+        eval_info += 'Missing classes in evaluation set: '
+        for c in missing_classes:
+            eval_info += 'Class {} (train counts {})'.format(c, self.train_class_counts[c])
+
+        return eval_info, class_acc.mean(), overall_acc
+
+    def evaluate(self, loader):
+
+        eval_info, eval_acc_mac, eval_acc_mic = self.evaluate_epoch(loader)
+        self.logger.info(eval_info)
+        self.logger.info('Macro Acc: {:.3f}; Micro Acc: {:.3f}\n'.format(eval_acc_mac * 100, eval_acc_mic * 100))
+
+        return eval_acc_mac
+
+    def save_model(self):
+        os.makedirs(self.weights_path.rsplit('/', 1)[0], exist_ok=True)
+        self.logger.info('Saving to {}'.format(self.weights_path))
+        self.net.save(self.weights_path)
